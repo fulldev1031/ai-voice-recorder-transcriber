c0334234e6cd0c300f53a445da37ced10d23cd46
Gaurav Karakoti <karakotigaurav12@gmail.com>
2025-02-27 14:09:35 +0530

    resolving Conflicts

---
+++ app/gui/components/log_handler.py
@@ -0,0 +1,13 @@
+import logging
+import tkinter as tk
+class TextBoxLogHandler(logging.Handler):
+    def __init__(self, text_widget):
+        super().__init__()
+        self.text_widget = text_widget
+
+    def emit(self, record):
+        msg = self.format(record)
+        self.text_widget.config(state=tk.NORMAL)
+        self.text_widget.insert(tk.END, msg + '\n')
+        self.text_widget.config(state=tk.DISABLED)
+        self.text_widget.see(tk.END)
